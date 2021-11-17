77900cf58651e6444deec6d92bfc4a6c1c6dfaf8
Matej Cotman <matej.cotman@eficode.com>
2021-11-17 11:25:18 +0000

    refactor(novnc): use official image for Browser library

--- novnc/robot.sh
+++ novnc/robot.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-PATH="/home/user/venv/bin:$PATH"
+PATH="/home/pwuser/.local/bin:$PATH"
 ROBOT_OUTPUT="${ROBOT_OUTPUT:-/usr/share/novnc/output}"
-ROBOT_ROOT_PATH="${ROBOT_ROOT_PATH:-/home/user/source}"
+ROBOT_ROOT_PATH="${ROBOT_ROOT_PATH:-/home/pwuser/source}"
 
 # This will take down the whole process tree on script exit
 trap "kill 0" EXIT
 trap "exit" INT TERM
 
-while true; do
-    fswatch --event=Updated --one-event $ROBOT_ROOT_PATH/**/*robot >/dev/null
-    robot -d $ROBOT_OUTPUT $ROBOT_ROOT_PATH/tests || true
-done
+if [ -z "$ROBOT_RUN_ONCE" ]
+then
+
+    while true
+    do
+        fswatch --event=Updated --insensitive --exclude='.*\.log$' --recursive --one-event $ROBOT_ROOT_PATH >/dev/null
+        robot -d $ROBOT_OUTPUT $ROBOT_ROOT_PATH/tests || true
+    done
+
+else
+
+    robot -d $ROBOT_OUTPUT $ROBOT_ROOT_PATH/tests
+
+fi
