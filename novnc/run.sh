083641646700a7b358d7573d91256c2b33b835ac
Matej Cotman <matej.cotman@eficode.com>
2021-11-17 11:25:18 +0000

    refactor(novnc): use official image for Browser library

--- novnc/run.sh
+++ novnc/run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-chown -R user:user /home/user/.config
+chown -R pwuser:pwuser /home/pwuser/.config
 
-chmod -R o+rw /home/user/source
+chmod -R o+rw /home/pwuser/source
 
-mkdir -p /home/user/logs
-chmod -R o+rw /home/user/logs
+mkdir -p /home/pwuser/logs
+chmod -R o+rw /home/pwuser/logs
 
 mkdir -p /usr/share/novnc/output
 chmod -R o+rw /usr/share/novnc/output
@@ -14,4 +14,4 @@ chmod -R o+rw /usr/share/novnc/output
 trap 'kill $(cat /tmp/supervisord.pid)' EXIT
 trap 'exit 0' SIGINT SIGTERM
 
-exec supervisord --nodaemon --pidfile=/tmp/supervisord.pid --configuration=/etc/supervisord.conf
+exec supervisord --configuration=/etc/supervisord.conf
